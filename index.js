cbea4e2ad139578d84c2c36a61240c1378e14a12
Valentin Bordeianu <blenminer123@gmail.com>
2023-01-19 16:28:17 +0100

    testing

--- index.js
+++ index.js
@@ -2,7 +2,52 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('cross-fetch');
 
-const endpoit = "https://build-api.cloud.unity3d.com/api/v1";
+const endpoit = "https://build-api.cloud.unity3d.com/api/v1/orgs/20066711958695";
+
+async function InitProject(cloudToken, repoName, repoSSHUrl)
+{
+    let url = `${endpoit}/projects`;
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${cloudToken}`
+        },
+        body: JSON.stringify({
+            name: repoName,
+            disabled: false,
+            disableNotifications: true,
+            generateShareLinks: true,
+            settings: {
+                remoteCacheStrategy: "library",
+                scm: {
+                    type: "git",
+                    url: repoSSHUrl
+                }
+            }
+        })
+    });
+
+    let json = await response.json();
+}
+
+async function RebuildLauncher(cloudToken, prod) 
+{
+    let url = `${endpoit}/projects/ig-launcher${prod ? "-prod" : ""}/buildtargets/_all/builds`;
+
+    await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${cloudToken}`
+        },
+        body: JSON.stringify({
+            clean: false,
+            delay: 0
+        })
+    });
+}
 
 async function run()
 {
@@ -10,25 +55,28 @@ async function run()
         try
         {
             const cloudToken = core.getInput('token', {required: true});
+            const mode = core.getInput('mode', {required: false});
 
-            let url = `${endpoit}/orgs/20066711958695/projects/ig-launcher/buildtargets/_all/builds`;
-            
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${cloudToken}`
-                },
-                body: JSON.stringify({
-                    clean: false,
-                    delay: 0
-                })
-            });
+            await InitProject(
+                cloudToken,
+                github.context.payload.repository.name,
+                github.context.payload.repository.ssh_url
+            );
 
-            let json = await response.json();
-
-            core.info(JSON.stringify(json));
-            core.info(JSON.stringify(github));
+            /*if (mode == "Init")
+            {
+                await InitProject(
+                    github.context.payload.repository.name,
+                    github.context.payload.repository.ssh_url
+                );
+            }
+            else
+            {
+                await RebuildLauncher(
+                    cloudToken, 
+                    github.context.payload.ref === "refs/heads/prod"
+                );
+            }*/
         }
         catch (error)
         {
